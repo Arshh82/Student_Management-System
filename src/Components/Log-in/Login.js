31d4eab91f6e856e6b1a72c4b7bd13bcfd2ac558
Arshh82 <arshilbaig82@gmail.com>
2023-08-01 17:29:04 +0530

    firsst

---
+++ src/Components/Log-in/Login.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Login.css'
+
+
+// let bgimage = new URL ("/public/images/login/pexels-karl-solano-2883049.jpg",import.meta.url)
+
+const Login = () => {
+  return (
+    <>
+      <div className='login-container'>
+        <div className='login-containe'>
+        <div className='login-heading'>
+        <p className='heading-label'>Sig in</p>
+
+        </div>
+          <div className='login-sec'>
+            <div class="mb-2">
+              <label for="exampleFormControlInput1" class="form-label">User name</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" />
+            </div>
+            <div className="mb-4">
+              <label for="exampleFormControlInput1" class="form-label">Password</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1"  />
+            </div>
+            <div className="mt-3">
+            <button type="button" className="botn ">SIGN IN</button>
+            </div>
+            <div className="mt-5">
+              <p className='siguptext'>Dont Have account ? <button type="button" className="btn-2 ">Sign up</button> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
