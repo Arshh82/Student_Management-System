0f2ecf621f59b966d1b5aea4688e18b703cdcd60
Arshh82 <arshilbaig82@gmail.com>
2023-08-02 14:01:34 +0530

    first

--- src/Components/Log-in/Login.js
+++ src/Components/Log-in/Login.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 
 // let bgimage = new URL ("/public/images/login/pexels-karl-solano-2883049.jpg",import.meta.url)
 
 const Login = () => {
+  let navigate = useNavigate();
+  let [data, updatedata] = useState({ name: "", pwd: "" });
+  function change(e) {
+    updatedata({ ...data, [e.target.name]: e.target.value });
+  }
   return (
     <>
       <div className='login-container'>
@@ -13,23 +21,53 @@ const Login = () => {
           <div className='login-heading'>
             <p className='heading-label'>Sig in</p>
           </div>
+          <form  onSubmit={(e) => {
+            e.preventDefault();
+            async function signup() {
+              let d = await axios.get(
+                "https://student-api-34v1.onrender.com/users/"
+              );
+              // console.log(d);
+              // alert('hii')
+              d = await d.data;
+              let c = 0;
+              for (var i of d) {
+                if (data.name === i.name && data.pwd === i.pwd) {
+                  c++;
+                  break;
+                }
+              }
+              if (c === 0) {
+                alert("Invalid user id and password");
+              } else {
+                alert("Log In Successfull");
+                navigate("/signup");
+              }
+            }
+            signup();
+          }}
+        >
           <div className='login-sec'>
             <div class="mb-2">
               <label for="exampleFormControlInput1" class="form-label">User name</label>
-              <input type="text" class="form-control" id="exampleFormControlInput1" />
+              <input type="text" class="form-control" id="exampleFormControlInput1" value={data.name}  type="name"
+              name="name" onChange={change} />
             </div>
             <div className="mb-4">
               <label for="exampleFormControlInput1" class="form-label">Password</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" />
+              <input type="text" class="form-control" id="exampleFormControlInput1" value={data.pwd}  type="pwd"
+              name="pwd"   onChange={change}/>
             </div>
             <div className="mt-3">
-              <button type="button" className="botn ">SIGN IN</button>
+              <button type="submit" className="botn ">SIGN IN</button>
             </div>
             <div className="mt-5">
               <p className='siguptext'>Dont Have account ? <Link to='/signup'><button type="button" className="btn-2 ">Sign up</button></Link> </p>
             </div>
           </div>
+          </form>
         </div>
+        
       </div>
     </>
   )
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,42 +1,83 @@
 import React from 'react';
 import './Signup.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
+    let navigate = useNavigate()
+    // let [data, updatedata] = useState([]);
+    let [user, updateUser] = useState({ name: '', email: '', contact: '', pwd: '' });
+    // useEffect(() => {
+    //     show();
+    // });
+    // async function show() {
+    //     var res = await axios.get("https://student-api-34v1.onrender.com/users/");
+    //     // console.log(res)
+    //     updatedata(res.data);
+    // }
+    function change(e) {
+        updateUser({ ...user, [e.target.name]: e.target.value });
+    }
+
+
   return (
       <>
           <div className='signup-container'>
               <div className='signup-containe'>
                   <div className='signup-sec'>
-                  <div className='mb-2'>
-                    <h3>Sign Up</h3>
-                  </div>
-                  <div className='mb-5'>
-                    <label>Enter your email and password to register</label>
-                  </div>
+                      <div className='mb-2'>
+                          <h3>Sign Up</h3>
+                      </div>
+                      <div className='mb-5'>
+                          <label>Enter your email and password to register</label>
+                      </div>
+                      <form onSubmit={(e) => {
+            e.preventDefault();
+            async function addData() {
+              var res = await axios.post(
+                "https://student-api-34v1.onrender.com/users/",
+                user
+              );
+              alert("USer Sign Up Completed");
+              navigate('/');
+              
+            }
+            addData();
+          }}
+        >
                       <div class="mb-3">
-                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder='Name' />
+                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder='Name' name="name"
+                    value={user.name}
+                    onChange={change} />
                       </div>
                       <div class="mb-3">
-                          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder='Email' />
+                          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder='Email' name="email"
+                    value={user.email}
+                    onChange={change} />
                       </div>
                       <div class="mb-3">
-                          <input type="number" class="form-control" id="exampleFormControlInput1" placeholder='Contact number' />
+                          <input type="number" class="form-control" id="exampleFormControlInput1" placeholder='Contact number' name="contact"
+                    value={user.contact}
+                    onChange={change} />
                       </div>
                       <div class="mb-3">
-                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder='Password' />
+                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder='Password' name="pwd"
+                    value={user.pwd}
+                    onChange={change} />
                       </div>
                       <div className="form-check  mb-3">
-                          <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked"  />
+                          <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
                           <label class="form-check-label" for="flexCheckChecked">
-                          I agree the Terms and Conditions
+                              I agree the Terms and Conditions
                           </label>
                       </div>
                       <div className="mt-1">
-                          <button type="button" className="botn ">SIGN UP</button>
+                          <button type="submit" className="botn ">SIGN UP</button>
                       </div>
+                      </form>
                       <div className="mt-4">
-                          <p className='siguptext'>Already have an account?<Link to='/signup'><button type="button" className="btn-2 ">Sign in</button></Link> </p>
+                          <p className='siguptext'>Already have an account?<Link to='/'><button type="button" className="btn-2 ">Sign in</button></Link> </p>
                       </div>
                   </div>
               </div>
